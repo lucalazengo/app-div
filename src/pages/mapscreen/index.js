cbb24e47fdb12fae97148646524dbf5ead650b7a
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2023-11-16 01:03:32 -0300

    update

--- src/pages/mapscreen/index.js
+++ src/pages/mapscreen/index.js
@@ -1,31 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  TouchableOpacity,
-  TextInput,
-} from 'react-native'
-import MapView, {
-  Marker,
-  Callout,
-  PROVIDER_GOOGLE,
-  Polyline,
-} from 'react-native-maps'
+import React, { useState, useEffect, useRef } from 'react'
+import { StyleSheet, Text, View, Button, Animated } from 'react-native'
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 import * as Location from 'expo-location'
-import axios from 'axios'
 import { useRoute } from '@react-navigation/native'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import MapViewDirections from 'react-native-maps-directions'
 
 const MapScreen = () => {
   const route = useRoute()
-  const [currentLocation, setCurrentLocation] = useState(null)
+  const [currentLocation, setCurrentLocation] = useState()
   const [destination, setDestination] = useState(null)
-  const [routeCoordinates, setRouteCoordinates] = useState([])
-  const [duration, setDuration] = useState(null)
   const [isRouteOpen, setIsRouteOpen] = useState(false)
+  const [animation] = useState(new Animated.Value(0))
+  const [distance, setDistance] = useState('')
+  const [duration, setDuration] = useState('')
+
+  const startAnimation = () => {
+    Animated.timing(animation, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start()
+  }
 
   useEffect(() => {
     ;(async () => {
@@ -47,8 +43,9 @@ const MapScreen = () => {
     })()
   }, [route.params])
 
-  const handleStartRoute = () => {
+  const handleStartRoute = async () => {
     setIsRouteOpen(true)
+    startAnimation()
   }
 
   return (
@@ -66,16 +63,32 @@ const MapScreen = () => {
         showsMyLocationButton
         showsCompass
         toolbarEnabled
+        onUserLocationChange={(event) => {
+          setCurrentLocation(event.nativeEvent.coordinate)
+        }}
       >
         <MapViewDirections
           origin={currentLocation}
           destination={destination}
           apikey="AIzaSyDevAuVwiMi3DTNcKcMquHV2I-_EzjmJz8"
-          strokeWidth={3}
+          strokeWidth={6}
           strokeColor="hotpink"
           onReady={(result) => {
             console.log(`Distance: ${result.distance} km`)
             console.log(`Duration: ${result.duration} min.`)
+
+            const updatedDistance = `Distance: ${result.distance.toFixed(2)} km`
+            const updatedDuration = `Duration: ${result.duration.toFixed(
+              2,
+            )} min.`
+
+            if (distance !== updatedDistance) {
+              setDistance(updatedDistance)
+            }
+
+            if (duration !== updatedDuration) {
+              setDuration(updatedDuration)
+            }
           }}
         />
         {destination && (
@@ -101,14 +114,7 @@ const MapScreen = () => {
         )}
       </MapView>
 
-      <View
-        style={{
-          position: 'absolute',
-          bottom: 50,
-          left: 10,
-          right: 10,
-        }}
-      >
+      <View style={styles.searchContainer}>
         <GooglePlacesAutocomplete
           placeholder="Insira o destino"
           onPress={(data, details = null) => {
@@ -134,21 +140,60 @@ const MapScreen = () => {
             },
           }}
         />
-        <View
-          style={{
-            marginTop: 10,
-            marginBottom: 10,
-            backgroundColor: '#510E16',
-          }}
-        >
+        <View style={styles.startRouteButton}>
           <Button
             title="Iniciar rota"
-            onPress={handleStartRoute}
             color="black"
+            onPress={handleStartRoute}
           />
         </View>
+        {isRouteOpen && (
+          <Animated.View style={styles.routeInfoContainer}>
+            <Text>{distance}</Text>
+            <Text>{duration}</Text>
+          </Animated.View>
+        )}
       </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    position: 'absolute',
+    bottom: 50,
+    left: 10,
+    right: 10,
+  },
+  startRouteButton: {
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: '#510E16',
+  },
+  routeInfoContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    padding: 10,
+    borderRadius: 10,
+    transform: [
+      {
+        translateY: 0,
+      },
+      {
+        translateY: 0,
+      },
+      {
+        translateY: 0,
+      },
+      {
+        scale: 1,
+      },
+      {
+        perspective: 1000,
+      },
+    ],
+  },
+})
+
 export default MapScreen
